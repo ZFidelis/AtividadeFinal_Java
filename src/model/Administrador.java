88d4d7e918dc045a9d95b4277ea81c468bc6a3c4
ZFidelis <isaaczapo@hotmail.com>
2024-06-18 16:33:58 -0300

    model classes created with constructors, getters and setters

---
+++ src/model/Administrador.java
@@ -0,0 +1,27 @@
+package model;
+//#region Import
+import controller.*;
+import model.*;
+import view.*;
+//#endregion
+
+public class Administrador extends Usuario{
+    private int nivelAcesso;
+
+    //#region Constructors
+    public Administrador(String nome, String email, String senha, int nivelAcesso) {
+        super(nome, email, senha);
+        this.nivelAcesso = nivelAcesso;
+    }
+    //#endregion
+    //#region Getters & Setters
+    public int getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(int nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+    //#endregion
+    
+}
