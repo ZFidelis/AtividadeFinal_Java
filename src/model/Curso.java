647ed6ced87f86648b242f7b565b5c770077ac77
ZFidelis <isaaczapo@hotmail.com>
2024-06-19 14:49:04 -0300

    toString( ) and fromString( ) creation

---
+++ src/model/Curso.java
@@ -0,0 +1,80 @@
+package model;
+//#region Import
+import controller.*;
+import model.*;
+import view.*;
+import java.util.List;
+import java.util.ArrayList;
+//#endregion
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int duracao;
+    private String nomeProfessor;
+
+    //#region Constructors
+    public Curso() {
+
+    }
+
+    public Curso(String titulo, String descricao, int duracao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.duracao = duracao;
+    }
+    
+    public Curso(String titulo, String descricao, int duracao, String nomeProfessor) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.duracao = duracao;
+        this.nomeProfessor = nomeProfessor;
+    }
+    //#endregion
+    //#region Getter & Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setProfessor(String professor) {
+        this.nomeProfessor = professor;
+    }
+    //#endregion
+    
+    //#region String Manipulation
+    @Override
+    public String toString() {
+        return titulo + ", " + descricao + ", " + duracao + ", " + nomeProfessor;
+    }
+
+    public static Curso fromString(String linha) {
+        String[] dadosCurso = linha.split(", ");
+
+        return new Curso(dadosCurso[0], dadosCurso[1], Integer.parseInt(dadosCurso[2]), dadosCurso[3]);
+    }
+    //#endregion
+}
