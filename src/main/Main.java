fab9e71fc0dd5cc0fab2a6937592279e9e4b6e88
ZFidelis <isaaczapo@hotmail.com>
2024-06-21 14:37:36 -0300

    Main class updated

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,20 @@
 package main;
 //#region Import
-// import controller.*;
-// import model.*;
+import controller.*;
+import model.*;
 import view.*;
 //#endregion
 
 public class Main {
     public static void main(String[] args) {
-        int val = Console.gerarCodigoVerificacao();
-        System.out.println(val);
+        Sistema.inicializar();
+        int selecao = 1;
+        
+        while(selecao != 0) {
+            Sistema.menuPrincipal();
+            selecao = Console.lerInt();
+            Sistema.direcionarOpcaoPrincipal(selecao);
+
+        }
     }
 }
