53c6b8da6fcf7fb485138aaecbbd356150443164
Brian0505 <briancris2003@gmail.com>
2024-06-23 22:40:14 -0300

    Método Criar Curso ADM

--- src/model/Administrador.java
+++ src/model/Administrador.java
@@ -25,6 +25,30 @@ public class Administrador extends Usuario {
     //#endregion
     //#region Methods
     public void criarCurso() {
+        
+        System.out.println("\nCRIAÇÃO DE NOVO CURSO");
+
+   
+    System.out.print("Informe o título do curso: ");
+    String titulo = Console.lerString();
+
+    System.out.print("Informe a descrição do curso: ");
+    String descricao = Console.lerString();
+
+    System.out.print("Informe a duração do curso (em horas): ");
+    int duracao = Console.lerInt();
+
+    
+    String nomeProfessor = this.getNome(); 
+
+    
+    Curso curso = new Curso(titulo, descricao, duracao, nomeProfessor);
+
+    
+    GerenciadorCursos.adicionarCurso(curso);
+
+    System.out.println("\nCurso criado com sucesso!");
+
 
     }
 
