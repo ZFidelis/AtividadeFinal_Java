1fb995daccf9aa852a0310708614b4f1e1c72368
ZFidelis <isaaczapo@hotmail.com>
2024-06-23 23:30:37 -0300

    functional changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,32 @@
 package main;
 //#region Import
-// import controller.*;
-// import model.*;
+import controller.*;
+import model.*;
 import view.*;
 //#endregion
 
 public class Main {
     public static void main(String[] args) {
-        int val = Console.gerarCodigoVerificacao();
-        System.out.println(val);
+        Sistema.inicializar();
+        
+        // Aluno a = new Aluno("Isaac", "isaac@h", "icaas", 123);
+        // Aluno b = new Aluno("Isaac", "isaac@h", "icaas", 1234);
+        // GerenciadorAlunos.adicionarAluno(a);
+        // GerenciadorAlunos.adicionarAluno(b);
+        // System.out.println(GerenciadorCursos.getListaCursos());
+        // Curso c1 = new Curso("Aulas Java", "Aulas sobre backend intermedi rio utilizando Java como linguagem de programa  o", 220, "Jason Sobreiro");
+        // Curso c = new Curso("Design Software", "Aula sobre design de software", 230, "O Melhor");
+        // GerenciadorCursos.adicionarCurso(c1);
+        // GerenciadorCursos.adicionarCurso(c);
+        // a.entrarCurso("Aulas Java");
+        // b.entrarCurso("Aulas Java");
+        // b.entrarCurso("Design Software");
+        // System.out.println(GerenciadorCursos.getListaCursos());
+        Curso c = GerenciadorCursos.buscarCursos("Aulas Java");
+        Curso c1 = GerenciadorCursos.buscarCursos("Design Software");
+        // Aluno a = GerenciadorAlunos.buscarAluno(123);
+        c.mostrarAlunos();
+        c1.mostrarAlunos();
+        Sistema.finalizar();
     }
 }
