4c4cda811ae23ba82601061808e3aaa6f91ddae3
ZFidelis <144962808+ZFidelis@users.noreply.github.com>
2024-06-24 23:30:56 -0300

    Revert "merging all code to main"

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,13 @@
 package main;
 //#region Import
-import controller.*;
-import model.*;
+// import controller.*;
+// import model.*;
 import view.*;
 //#endregion
 
 public class Main {
     public static void main(String[] args) {
-        Sistema.inicializar();
-        Sistema.executar();
-        Sistema.finalizar();
+        int val = Console.gerarCodigoVerificacao();
+        System.out.println(val);
     }
 }
